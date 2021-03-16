992b896a781f1159142ec3813e8f74059cc539ff
Simon Pannek <simon.pannek@tum.de>
2021-03-16 18:51:53 +0100

    Add clear command

---
+++ commands/misc/clear.js
@@ -0,0 +1,27 @@
+module.exports = {
+    name: "clear",
+    description: "Deletes a certain amount of messages.",
+    usage: "[number]",
+    min_args: 1,
+    permissions: "ADMINISTRATOR",
+    async execute(message, args) {
+        // Check if argument is a number
+        if (isNaN(args[0])) {
+            throw new Error("Invalid arguments.");
+        }
+
+        const num = Number(args[0]);
+
+        // Check if number is in bounds
+        if (num >= 1 && num <= 100) {
+            // Delete messages
+            const deleted = await message.channel.bulkDelete(num + 1, true);
+
+            return message.channel.send(`Cleared ${deleted.size - 1} messages.`).then(sent => setTimeout(() => {
+                sent.delete();
+            }, 2500));
+        } else {
+            return message.channel.send("Number has to be between 1 and 100.")
+        }
+    }
+};
