51bb883de74b81ba38f18605c6384d80c316f450
Simon Pannek <simon.pannek@tum.de>
2021-03-18 18:50:30 +0100

    Add rank command

--- commands/reactions/rank.js
+++ commands/reactions/rank.js
@@ -1,10 +1,10 @@
-const {users} = require("../../modules/database");
+const {sequelize, users} = require("../../modules/database");
 const {userFromMention} = require("../../modules/parser");
 const {errorResponse} = require("../../modules/response");
 
 module.exports = {
-    name: "reactions",
-    description: "Queries the number of reactions a user has received.",
+    name: "rank",
+    description: "Queries the rank of a certain user.",
     usage: "[?user]",
     cooldown: 10,
     async execute(message, args) {
@@ -18,9 +18,14 @@ module.exports = {
         if (user) {
             // Try to get the reactions from the database
             try {
-                const reacted = await users.findOne({where: {guild: message.guild.id, user: user.id}});
-                if (reacted) {
-                    return message.channel.send(`The user ${user.tag} has **${reacted.get("reactions")} reaction(s)**.`);
+                const rank = await users.findOne({
+                    where: {guild: message.guild.id, user: user.id},
+                    attributes: [
+                        [sequelize.literal("(RANK() OVER (PARTITION BY 'guild' ORDER BY reactions DESC))"), "rank"]
+                    ]
+                });
+                if (rank) {
+                    return message.channel.send(`The user ${user.tag} is ranked **number ${rank.get("rank")}**.`);
                 } else {
                     return message.channel.send(`Could not find an entry for the user ${user.tag}.`);
                 }
