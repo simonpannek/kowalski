7c97b0c2c79d9ec2751a18f9c777f855af09af7d
Simon Pannek <simon.pannek@tum.de>
2021-03-23 14:22:03 +0100

    Minor changes

--- events/guildMemberRemove.js
+++ events/guildMemberRemove.js
@@ -3,6 +3,11 @@ const {users} = require("../modules/database");
 module.exports = {
     name: "guildMemberRemove",
     async execute(member) {
+        // Check if user is a bot
+        if (member.user.bot) {
+            return;
+        }
+
         // Remove database entry of user
         return users.destroy({where: {guild: member.guild.id, user: member.user.id}});
     }
--- events/roleDelete.js
+++ events/roleDelete.js
@@ -3,6 +3,11 @@ const {roles, reactionroles} = require("../modules/database");
 module.exports = {
     name: "roleDelete",
     async execute(role) {
+        // Check if it is a managed role
+        if (role.managed) {
+            return;
+        }
+
         // Remove database entries of role
         await roles.destroy({where: {guild: role.guild.id, role: role.id}});
         return reactionroles.destroy({where: {guild: role.guild.id, role: role.id}});
